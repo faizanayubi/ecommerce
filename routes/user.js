8521e494d78a12d5930252c9d093f88faaa91722
Faizan Ayubi <indianayubi@gmail.com>
2016-06-26 22:47:15 +0530

    Signup page created

---
+++ routes/user.js
@@ -0,0 +1,27 @@
+var router = require('express').Router();
+var User = require('../models/user');
+
+router.get('/signup', function(req, res, next) {
+	res.render('accounts/signup');
+});
+
+router.post('/signup', function(req, res, next) {
+	var user = new User();
+	user.profile.name = req.body.name;
+	user.password = req.body.password;
+	user.email = req.body.email;
+
+	user.findOne({email: req.body.email}, function (err, existingUser) {
+		if (existingUser) {
+			console.log(req.body.email + " already exists");
+			return res.redirect("/signup");
+		} else {
+			user.save(function(err, next) {
+				if (err) {return next(err);}
+				res.json('Successfully created new user');
+			});
+		}
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,31 +15,17 @@ mongoose.connect('mongodb://root:abc123@ds023054.mlab.com:23054/ecommerce', func
 })
 
 //Middleware
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
-app.post('/create-user', function(req, res, next) {
-	var user = new User();
-	user.profile.name = req.body.name;
-	user.password = req.body.password;
-	user.email = req.body.email;
-
-	user.save(function(err, next) {
-		if (err) {return next(err);}
-		res.json('Successfully created new user');
-	});
-});
-
-app.get('/', function(req, res) {
-	res.render('main/home.ejs');
-});
-
-app.get('/about', function(req, res) {
-	res.render('main/about.ejs');
-});
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+app.use(mainRoutes);
+app.use(userRoutes);
 
 app.listen(3000, function(err) {
 	if (err) {throw err;}
